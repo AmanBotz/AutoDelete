cfe215f43e44676cbc5a53b3d72f8fa36b409676
Aman Godz <amanpooniaff@gmail.com>
2025-05-04 17:06:04 +0530

    Update database.py

--- database.py
+++ database.py
@@ -2,12 +2,12 @@ from pymongo import MongoClient
 from config import MONGO_URI
 
 client = MongoClient(MONGO_URI)
-db = client["autodelete_bot"]
-settings_col = db["group_settings"]
+db = client["autodeleter"]
+settings = db["group_settings"]
 
 def get_group_delay(chat_id: int) -> int:
-    data = settings_col.find_one({"chat_id": chat_id})
-    return data["delay"] if data else None
+    doc = settings.find_one({"chat_id": chat_id})
+    return doc["delay"] if doc else None
 
 def set_group_delay(chat_id: int, delay: int):
-    settings_col.update_one({"chat_id": chat_id}, {"$set": {"delay": delay}}, upsert=True)
+    settings.update_one({"chat_id": chat_id}, {"$set": {"delay": delay}}, upsert=True)
